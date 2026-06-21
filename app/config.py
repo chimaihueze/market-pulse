b42e4d35312734793bfcd6be21c8148157e40619
chimaihueze <kihueze@gmail.com>
2026-06-21 12:22:25 +0100

    updated Settings

--- app/config.py
+++ app/config.py
@@ -1,12 +1,14 @@
 
 class Settings:
     def __init__(self):
-        self.binance_ws_url = "wss://stream.binance.com:9443/stream?streams=btcusdt@trade/ethusdt@trade"
-        self.ws_ping_interval = 20
-        self.ws_ping_timeout = 20
-        self.backoff_base = 1
-        self.backoff_cap = 30
+        self.binance_ws_url: str = "wss://stream.binance.com:9443/stream?streams=btcusdt@trade/ethusdt@trade"
+        self.ws_ping_interval: int = 20
+        self.ws_ping_timeout: int = 20
+        self.backoff_base: int = 1
+        self.backoff_cap: int = 30
 
-        self.symbol = ["btcusdt", "ethusdt"]
+        self.max_clock_drift_seconds: int = 30
+
+        self.symbol: list = ["btcusdt", "ethusdt"]
 
 settings = Settings()
