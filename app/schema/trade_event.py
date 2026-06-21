f0912c1aab500fdedfdfab334a386b35e5b4d25c
chimaihueze <kihueze@gmail.com>
2026-06-21 01:14:19 +0100

    added trade event

---
+++ app/schema/trade_event.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from decimal import Decimal
+
+from pydantic import BaseModel
+
+
+class TradeEvent(BaseModel):
+    event_version: int = 1
+    event_type: str = "trade"
+
+    event_id: str
+
+    exchange: str
+    symbol: str
+
+    trade_id: str
+
+    price: Decimal
+    quantity: Decimal
+
+    trade_timestamp: datetime
+    ingest_timestamp: datetime
+
+    is_buyer_maker: bool
