e311ce014da377cce8fb4aa89c4b3b484f724400
chimaihueze <kihueze@gmail.com>
2026-06-21 23:56:53 +0100

    added self.kafka_url

--- app/config/settings.py
+++ app/config/settings.py
@@ -2,6 +2,7 @@
 class Settings:
     def __init__(self):
         self.binance_ws_url: str = "wss://stream.binance.com:9443/stream?streams=btcusdt@trade/ethusdt@trade"
+        self.kafka_url: str = "localhost:9092"
         self.ws_ping_interval: int = 20
         self.ws_ping_timeout: int = 20
         self.backoff_base: int = 1
