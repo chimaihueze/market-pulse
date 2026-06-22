67f1adf42fd221584393e92458b724d8fbe5685a
chimaihueze <kihueze@gmail.com>
2026-06-22 23:20:29 +0100

    updated main file

--- app/main.py
+++ app/main.py
@@ -1,6 +1,66 @@
-def main():
-    print("Hello from market-pulse!")
+import asyncio
+
+from loguru import logger
+
+from app.config.settings import settings
+from app.ingestion.binance_ws_client import BinanceWSClient
+from app.ingestion.consumer import Consumer
+from app.pipelines.pipeline import TradePipeline
+from app.producers.kafka_producer import KafkaProducer
+from app.validators.trade_validator import TradeValidator
+from observability.logger import setup_logger
+
+
+async def main():
+    logger.info("starting market-pulse")
+
+    producer = KafkaProducer(settings.kafka_url)
+
+    started = False
+
+    for i in range(3):
+        try:
+            await producer.start()
+            started = True
+            logger.info("kafka connected")
+            break
+        except Exception as e:
+            logger.error(f"kafka retry {i}", extra={"error": str(e)})
+            await asyncio.sleep(2 ** i)
+
+    if not started:
+        logger.critical("kafka unavailable - shutting down service")
+        await producer.stop()
+        return
+
+    validator = TradeValidator(settings)
+
+    pipeline = TradePipeline(
+        validator=validator,
+        publisher=producer
+    )
+
+    ws_client = BinanceWSClient(
+        url=settings.binance_ws_url,
+        ping_interval=settings.ws_ping_interval,
+        ping_timeout=settings.ws_ping_timeout,
+    )
+
+    consumer = Consumer(
+        ws_client=ws_client,
+        pipeline=pipeline,
+    )
+
+    try:
+        await consumer.run()
+    finally:
+        logger.info("shutting down producer")
+        await producer.stop()
 
 
 if __name__ == "__main__":
-    main()
+    setup_logger()
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        logger.warning("shutting down cleanly")
