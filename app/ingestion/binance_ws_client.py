3fbd9f4ecce63881510d916a08ec64a306d1fab5
chimaihueze <kihueze@gmail.com>
2026-06-27 00:30:12 +0100

    updated BinanceWSClient

--- app/ingestion/binance_ws_client.py
+++ app/ingestion/binance_ws_client.py
@@ -3,12 +3,15 @@ import websockets
 from websockets.exceptions import ConnectionClosed
 from loguru import logger
 
+from app.core.backoff import Backoff
+
 
 class BinanceWSClient:
     def __init__(self, url: str, ping_interval: int, ping_timeout: int):
         self.url = url
         self.ping_interval = ping_interval
         self.ping_timeout = ping_timeout
+        self.backoff = Backoff()
 
     async def connect(self):
         while True:
@@ -19,15 +22,20 @@ class BinanceWSClient:
                     ping_timeout=self.ping_timeout,
                 ) as ws:
 
-                    logger.info("Connected to Binance WebSocket")
+                    logger.info("binance websocket connected")
+                    self.backoff.success()
 
                     async for raw in ws:
                         yield raw
 
             except ConnectionClosed as e:
-                logger.warning(f"WebSocket closed: {e}")
-                await asyncio.sleep(2)
+                logger.exception(f"binance websocket closed: {e}")
+                self.backoff.failure()
+                delay = self.backoff.next_delay()
+                await asyncio.sleep(delay)
 
             except Exception as e:
-                logger.error(f"WebSocket error: {e}")
-                await asyncio.sleep(5)
+                logger.exception(f"binance websocket error: {e}")
+                self.backoff.failure()
+                delay = self.backoff.next_delay()
+                await asyncio.sleep(delay)
